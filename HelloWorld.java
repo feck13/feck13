c54a236c7ea86a0f2c28356353580033a8665082
feck13 <feck13@hotmail.it>
2019-12-07 12:24:11 +0100

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	   System.out.println("Hello World!");
+	   System.out.println("Ragazzi");
+	   System.out.print("ciao");
+	   System.out.print(100+187);
+	   }
+	  
+	   
+	   }
